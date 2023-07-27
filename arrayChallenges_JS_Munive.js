10a09b8e06b1d5d557537e5ac6c7de4c1ff848bc
roberta-munive <114707010+roberta-munive@users.noreply.github.com>
2023-07-27 10:09:06 -0500

    Added alternative solution to reverse array

--- arrayChallenges_JS_Munive.js
+++ arrayChallenges_JS_Munive.js
@@ -96,6 +96,27 @@ var result = reverse(["a", "b", "c", "d", "e"]);
 console.log(result); // we expect back ["e", "d", "c", "b", "a"]
 console.log("-----------------------------------------");
 
+//*****Array Reverse - Alternative Solution*****
+/* Write a function that will reverse the values of an array and return them*/
+
+console.log("*****Array Reverse - Alternative Solution*****");
+
+function arrReverse(arr){
+    for(var i=0; i < arr.length/2; i++){
+        let temp = arr[arr.length - 1 - i];
+        arr[arr.length - 1 - i] = arr[i];
+        arr[i] = temp;
+    }
+
+    return arr;
+}
+
+var result = arrReverse(["a", "b", "c", "d", "e"]);
+console.log(result); // we expect back ["e", "d", "c", "b", "a"]
+result = arrReverse([1, 2, 3, 4, 5]);
+console.log(result); // we expect back [5, 4, 3, 2, 1]
+console.log("-----------------------------------------");
+
 //*****Fibonacci Array*****
 /* Write a function that will return an array of Fibonacci numbers up to a given length n. Fibonacci numbers are calculated by adding the last two values in the sequence together. So if the 4th value is 2 and the 5th value is 3 then the next value in the sequence is 5.*/
 
